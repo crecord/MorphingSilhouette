c83f924f6ba9c161cbf2667cfe5ac6c30df9eced
crecord <crecord@gmail.com>
2018-04-13 15:30:42 -0400

    small updates

--- bin/slack/dep.sh
+++ bin/slack/dep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt install python-pip
+echo y | sudo apt install python-pip
 pip install slackclient
 sudo apt install wmctrl
 crontab new.crontab
-COMP_NAME='Rogers' python /home/studio/Documents/of_v20170714_linux64gcc5_release/apps/myApps/MorphingSilhouette/bin/slack/postImageToSlack.py
+COMP_NAME='Smith' python /home/studio/Documents/of_v20170714_linux64gcc5_release/apps/myApps/MorphingSilhouette/bin/slack/postImageToSlack.py
